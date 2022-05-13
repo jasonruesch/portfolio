6034ba45ddee574b166a04e13d62d434e1aac496
Jason Ruesch <jason.ruesch@me.com>
2022-05-13 17:22:53 -0400

    feat: updated about text

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -6,14 +6,14 @@ const About = () => {
       <div className="w-full gap-2 md:max-w-screen-sm sm-max-h:max-w-lg">
         <h1>About me</h1>
         <p>
-          My name is Jason Ruesch, and I strive to create software that is
-          spectacular and easy to use. I have a passion for learning new
+          My name is Jason Ruesch, and I enjoy creating software that looks
+          amazing and easy to use! I have a passion for learning new
           technologies and building things that are useful to others.
         </p>
         {/* 1. 2. 3. Accomplishements */}
         <p>
-          In my spare time, I enjoy spending time with my family, playing video
-          games, and watching movies and tv shows.
+          Outside of work, I like to spend time with my family, play video
+          games, and watch movies and tv shows.
         </p>
         <p>
           I am always open to connect. If you are interested in learning more
