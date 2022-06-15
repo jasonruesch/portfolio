4d7408c0e4a4c9b935bda109b6030cc0616b487c
Jason Ruesch <jruesch@degreed.com>
2022-06-14 23:03:07 -0400

    feat: add crumble transition

--- apps/portfolio/components/PageTransitions.tsx
+++ apps/portfolio/components/PageTransitions.tsx
@@ -1,54 +1,34 @@
 import classNames from 'classnames';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import gsap from 'gsap';
 
 const PageTransitions = ({ children, route, routingPageOffset }) => {
   const [transitioning, setTransitioning] = useState(false);
-  const tl = useRef<gsap.core.Timeline>(null);
-  const transitionRef = useRef<HTMLDivElement>(null);
 
-  const playTransition = () => {
-    tl.current.play(0);
+  const onExitStart = (element) => {
     setTransitioning(true);
+    gsap
+      .timeline()
+      .fromTo(
+        element,
+        {
+          clipPath:
+            'polygon(1% 0%, 28% 0%, 69% 0%, 100% 1%, 100% 32%, 100% 68%, 100% 99%, 71% 100%, 34% 100%, 0% 99%, 0% 68%, 0% 29%)',
+        },
+        {
+          clipPath:
+            'polygon(26% 33%, 40% 36%, 56% 20%, 63% 33%, 72% 39%, 69% 49%, 69% 60%, 62% 75%, 50% 68%, 36% 73%, 25% 60%, 30% 45%)',
+          rotation: -10,
+          scale: 0.85,
+        },
+      )
+      .to(element, { yPercent: 100 });
   };
 
   const stopTransition = () => {
     setTransitioning(false);
   };
-
-  useEffect(() => {
-    if (!transitionRef.current) {
-      return;
-    }
-
-    gsap.set(transitionRef.current.children, { autoAlpha: 1 });
-    tl.current = gsap
-      .timeline({
-        repeat: 1,
-        repeateDelay: 0.2,
-        yoyo: true,
-        paused: true,
-      })
-      .fromTo(
-        transitionRef.current.children,
-        { scale: 0, borderRadius: '100%' },
-        {
-          scale: 1,
-          borderRadius: 0,
-          stagger: {
-            grid: 'auto',
-            from: 'edges',
-            ease: 'sine',
-            amount: 0.5,
-          },
-        },
-      );
-
-    return () => {
-      tl.current.kill();
-    };
-  }, []);
 
   return (
     <>
@@ -61,7 +41,7 @@ const PageTransitions = ({ children, route, routingPageOffset }) => {
           key={route}
           classNames="page"
           timeout={1000}
-          onEnter={playTransition}
+          onExit={onExitStart}
           onExited={stopTransition}
         >
           <div style={{ transformStyle: 'preserve-3d' }}>
@@ -69,14 +49,6 @@ const PageTransitions = ({ children, route, routingPageOffset }) => {
           </div>
         </CSSTransition>
       </TransitionGroup>
-      <div
-        ref={transitionRef}
-        className="page-grid fixed top-0 left-0 grid h-screen w-full"
-      >
-        {[...Array(100)].map((_, i) => (
-          <div key={i} />
-        ))}
-      </div>
 
       <style jsx global>{`
         .page-exit-active main {
